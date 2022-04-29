e939edfe8e6979741c1ac29c4dab3a8717ec9b27
unknown <naun.belo@trt14.jus.br>
2022-04-29 14:11:59 -0400

    0.1.14 - Criação de menu de apresentação para alteração do lado de apresentação da Raíra.

--- chatbot-plugin/chatbot-plugin.js
+++ chatbot-plugin/chatbot-plugin.js
@@ -4,8 +4,11 @@ const portalIframe = document.createElement("iframe")
 const chatBotMessage = document.createElement("div")
 const chatbotCall = document.createElement("div")
 const chatbotMessageClose = document.createElement("div")
+const positionSetter = document.createElement("div")
+let position = 'right'
 const chatbotState = {
     isShow: false,
+    menu: false,
     iframeSrc: "https://appserver1.trt14.jus.br/hml-chatbot/",
     personPic: "https://appserver1.trt14.jus.br/hml-chatbot/img/raira.d2ceab22.jpg",
     messages: [
@@ -18,12 +21,13 @@ const chatbotState = {
 async function chatbot(){
     portalDivIframe.classList.add('chatbot-div-iframe')
     portalDivIframe.classList.add('initial-position')
+    positionSetter.classList.add('position-setter')
     portalIframe.classList.add('chatbot-iframe')
     portalIframe.src = chatbotState.iframeSrc        
-    
     chatBotMessage.classList.add('chatbot-msg')
     chatBotMessage.classList.add('initial-position')
     chatBotMessage.innerHTML = chatbotState.messages[0].message
+    startPositionSetter()            
     
     chatbotCall.classList.add('chatbot-call')
     chatbotCall.classList.add('initial-position')
@@ -31,6 +35,7 @@ async function chatbot(){
     chatbotCall.addEventListener('click', async () => {
         if(!chatbotState.isShow){
             portalDivIframe.append(portalIframe)
+            portalDivIframe.append(positionSetter)
         }
         chatbotState.isShow = !chatbotState.isShow
         const classeAdd = chatbotState.isShow ? 'presentation-position' : 'initial-position'
@@ -52,6 +57,8 @@ async function chatbot(){
         chatbotState.countClick++
     })
 
+    setPosition(position)
+
     messageCloseMobile()
     
     portalBody.append(chatbotCall)
@@ -63,6 +70,49 @@ async function chatbot(){
     }, 1000)
 }
 
+function startPositionSetter(){
+    positionSetter.addEventListener('click', () => {
+        let _open = chatbotState.menu
+        chatbotState.menu = !chatbotState.menu
+        hamburguerMenu(_open)
+    })
+    hamburguerMenu()
+}
+
+function hamburguerMenu(open){
+    let _li = ''
+    if(position === 'left'){
+        _li = '<li><span onclick="setPosition(\'right\')">Reposicionar o chat para a direita</span></li>'
+    }else{
+        _li = '<li><span onclick="setPosition(\'left\')">Reposicionar o chat para a esquerda</span></li>'
+    }
+    positionSetter.innerHTML = `
+        <div>
+            <div class="menu-icon"></div>
+            <ul class="menu ${open ? 'chatbot-d-show' : 'chatbot-d-none'}">
+                ${_li}
+            </ul>
+        </div>
+    `
+
+}
+
+function setPosition(newPosition = 'left'){
+    position = newPosition
+
+    portalDivIframe.classList.remove('left')
+    portalDivIframe.classList.remove('right')
+    chatBotMessage.classList.remove('left')
+    chatBotMessage.classList.remove('right')
+    chatbotCall.classList.remove('left')
+    chatbotCall.classList.remove('right')
+
+    portalDivIframe.classList.add(position)
+    chatBotMessage.classList.add(position)
+    chatbotCall.classList.add(position)
+
+    hamburguerMenu()
+}
 
 function messageCloseMobile(){
     if(screen.width <= 480) {
